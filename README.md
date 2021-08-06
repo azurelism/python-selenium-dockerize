79571bd425df5f1a22e2367ea87624274fb5b37d
azurecin <azure@cinnamon.is>
2021-08-06 17:20:45 +0700

    add screenshots

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # azure-testing
+
+[![CircleCI](https://circleci.com/gh/azurelism/azure-testing.svg?style=shield&circle-token=277926b20a516528cc6e98b6d0daf052f9b3bfc9)](https://circleci.com/gh/azurelism/azure-testing)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
+import time
+import os
 
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
@@ -77,3 +79,23 @@ def init_driver(request):
         driver.close()
         driver.quit()
         driver = None
+
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """Capture screenshot on test case failure pytest-HTML Report"""
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            timestamp = time.strftime("%Y%m%d%H%M%S", time.localtime(time.time()))
+            screenshot = "screenshots" + os.sep + timestamp + '.png'
+            driver.get_screenshot_as_file(screenshot)
+            if screenshot:
+                html = '<div><img src="../%s" alt="screenshot" style="width:480px;height:222px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % screenshot
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
